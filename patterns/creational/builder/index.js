7167b2f28bb51698239e1e79bd5d2dc94f9c36dc
hurduring <idontwanttogoonthecart@gmail.com>
2016-06-14 17:35:38 +0300

    deleting console log

--- patterns/creational/builder/index.js
+++ patterns/creational/builder/index.js
@@ -2,4 +2,3 @@ var TournamentBuilder = require('./directors/director-1')
 var DogTournament = require('./tournaments/dog')
 
 var tournament = new TournamentBuilder().build(new DogTournament());
-console.log(tournament)
